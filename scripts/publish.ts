7da0232fe89fb072805dea763b415d4d5691e48f
ducng99 <tom@tomng.dev>
2025-12-30 02:41:39 +1300

    refactor: cleanup and updated publish action

---
+++ scripts/publish.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env bun
+
+import { $ } from "bun";
+import { glob } from "fs/promises";
+import { dirname, join } from "path";
+
+for await (const distPackageJson of glob("packages/*/dist/package.json")) {
+    const rootPackageJson = join(dirname(dirname(distPackageJson)), "package.json");
+    await Bun.write(rootPackageJson, Bun.file(distPackageJson));
+}
+
+await $`bun run --filter '*' pack`;
+
+for await (const tar of glob("tarballs/*")) {
+    await $`npm publish ${tar}`;
+}
